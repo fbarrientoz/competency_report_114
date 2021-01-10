c472ba2f0bd1338cff07b9d139f2eb3bae1b6a54
fbarrientoz <56286223+fbarrientoz@users.noreply.github.com>
2021-01-10 00:58:30 -0600

    Update s_linked_list.py

--- s_linked_list.py
+++ s_linked_list.py
@@ -1,65 +1,209 @@
-
 class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+    """
+    An object for storing a single node in a linked list
 
-    def __str__(self):
-        return str(self.val)
+    Attributes:
+        data: Data stored in node
+        next_node: Reference to next node in linked list
+    """
 
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next_node = next_node
 
-class LinkedList:
-    def __init__(self, val):
-        self.head = Node(val)
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
 
-    def __str__(self):
-        result = ""
-        tmp = self.head
-        while tmp:
-            result += str(tmp.val) + (' --> ' if tmp.next else '')
-            tmp = tmp.next
-        return result
+class SinglyLinkedList:
+    """
+    Linear data structure that stores values in nodes. The list maintains a reference to the first node, also called head. Each node points to the next node in the list
 
-    def append(self, val, after_node=None):
-        tmp = self.head
-        if not tmp:
+    Attributes:
+        head: The head node of the list
+    """
+
+    def __init__(self):
+        self.head = None
+        # Maintaining a count attribute allows for len() to be implemented in
+        # constant time
+        self.__count = 0
+
+    def is_empty(self):
+        """
+        Determines if the linked list is empty
+        Takes O(1) time
+        """
+
+        return self.head is None
+
+    def __len__(self):
+        """
+        Returns the length of the linked list
+        Takesn O(1) time
+        """
+
+        return self.__count
+
+    def add(self, data):
+        """
+        Adds new Node containing data to head of the list
+        Also called prepend
+        Takes O(1) time
+        """
+
+        new_head = Node(data, next_node=self.head)
+        self.head = new_head
+        self.__count += 1
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Returns the node or `None` if not found
+        Takes O(n) time
+        """
+
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding node at insertion point takes
+        O(n) time.
+        Takes overall O(n) time.
+        """
+
+        if index >= self.__count:
+            raise IndexError('index out of range')
+
+        if index == 0:
+            self.add(data)
             return
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
 
-        while tmp.next and not(after_node is None and tmp.val == after_node):
-            tmp = tmp.next
-        another = tmp.next
-        tmp.next = Node(val)
-        tmp.next.next = another
+            while position > 1:
+                current = current.next_node
+                position -= 1
 
-    def prepend(self, val):
-        nd = Node(val)
-        nd.next = self.head
-        self.head = nd
+            prev_node = current
+            next_node = current.next_node
 
-    def remove(self, value):
-        nd = self.head
-        if not nd:
-            return
+            prev_node.next_node = new
+            new.next_node = next_node
 
-        if nd.val == value:
-            self.head = nd.next
+        self.__count += 1
 
-        while nd.next:
-            if nd.next.val == value:
-                nd.next = nd.next.next
+    def node_at_index(self, index):
+        """
+        Returns the Node at specified index
+        Takes O(n) time
+        """
 
-            nd = nd.next
+        if index >= self.__count:
+            raise IndexError('index out of range')
+
+        if index == 0:
+            return self.head
+
+        current = self.head
+        position = 0
+
+        while position < index:
+            current = current.next_node
+            position += 1
+
+        return current
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or `None` if key doesn't exist
+        Takes O(n) time
+        """
+
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+                self.__count -= 1
+                return current
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+                self.__count -= 1
+                return current
+            else:
+                previous = current
+                current = current.next_node
+
+        return None
+
+    def remove_at_index(self, index):
+        """
+        Removes Node at specified index
+        Takes O(n) time
+        """
+
+        if index >= self.__count:
+            raise IndexError('index out of range')
+
+        current = self.head
+
+        if index == 0:
+            self.head = current.next_node
+            self.__count -= 1
+            return current
+
+        position = index
+
+        while position > 1:
+            current = current.next_node
+            position -= 1
+
+        prev_node = current
+        current = current.next_node
+        next_node = current.next_node
+
+        prev_node.next_node = next_node
+        self.__count -= 1
+
+        return current
 
 
-if __name__ == '__main__':
-    ts = LinkedList(6)
-    ts.append(10)
-    ts.append(21)
-    ts.append(12)
-    ts.append(33, 10)
+    def __iter__(self):
+        current = self.head
 
-    ts.prepend(-1)
+        while current:
+            yield current
+            current = current.next_node
 
-    ts.remove(10)
 
-    print(ts)
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        current = self.head
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return  '-> '.join(nodes)
